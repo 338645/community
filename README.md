ac71fa19cee602d82657689c9473b129d1118f67
338645 <1525409228@qq.com>
2022-06-14 15:53:29 +0800

    firstday-04

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 ##  Hacg Community
 
+##  框架
+
+后端框架：springboot
+
+数据库：mysql
+
+UI框架：bootstrap
+
+登录功能：GitHub-API
+
 ##  资料
 
 参考开源项目地址：https://github.com/codedrinker/community
 
 springboot开发指导文档：https://spring.io/guides
+
+gitHubApi登录功能网址：https://docs.github.com/cn/developers/apps/building-oauth-apps/creating-an-oauth-app
+
+bootstrap资料地址：https://v3.bootcss.com/components/
 
 vim-pc版安装教程：https://blog.csdn.net/weixin_54363263/article/details/120790827
 
--- src/main/java/com/hacg/community/comtroller/WebController.java
+++ src/main/java/com/hacg/community/comtroller/WebController.java
@@ -1,17 +1,30 @@
 package com.hacg.community.comtroller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * 项目关于web操作相关控制器
- * 前端路径：
- *
  * 后端路径：
+ *
+ * 前端路径：
+ *  1)../为上一目录
+ *  2)/为当前目录
+ *  3)//为前缀只加协议
+ *  1. dirname/ 这种路径，浏览器会讲地址栏的url和该路径拼接。
+ * 2.  /dirname 这种路径，浏览器会将协议、主机名和该路径拼接。
+ * 3. ./dirname 这种路径，浏览器会将地址栏的url和该路径拼接。
+ * 4. //dirname 这种路径，浏览器会将协议头和该api拼接。
  */
 
 @Controller
 public class WebController {
 
+    //默认访问首页controller
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 
 
 }
