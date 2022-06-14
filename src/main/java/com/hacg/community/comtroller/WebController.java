679892ef8a393a69da5a2570a9bab687ce0b32e3
su1jia1le19 <1525409228@qq.com>
2022-06-14 14:21:39 +0800

    firstday-01

---
+++ src/main/java/com/hacg/community/comtroller/WebController.java
@@ -0,0 +1,17 @@
+package com.hacg.community.comtroller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 项目关于web操作相关控制器
+ * 前端路径：
+ *
+ * 后端路径：
+ */
+
+@Controller
+public class WebController {
+
+
+
+}
