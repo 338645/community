42add769a9c44913f8a7417889c16b24537b0e51
338645 <1525409228@qq.com>
2022-07-02 20:46:03 +0800

    2022-7-2 20:45

--- src/main/java/com/hacg/community/Application.java
+++ src/main/java/com/hacg/community/Application.java
@@ -1,10 +1,11 @@
 package com.hacg.community;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.hacg.community.mapper"})
 public class Application {
 
     public static void main(String[] args) {
